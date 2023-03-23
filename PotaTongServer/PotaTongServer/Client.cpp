e2e56104944b219d8ed950e3308e209701102095
Son Meenki <meenki98@gmail.com>
2023-03-23 10:52:40 +0900

    server task

--- PotaTongServer/PotaTongServer/Client.cpp
+++ PotaTongServer/PotaTongServer/Client.cpp
@@ -9,10 +9,27 @@ Client::~Client()
 {
 }
 
-void Client::SendPacket()
+void Client::SendPacket(void* packet)	
 {
+	OverlappedEx* overlappedEx = new OverlappedEx{ reinterpret_cast<char*>(packet) };
+	WSASend(socket, &overlappedEx->wsaBuf, 1, 0, 0, &overlappedEx->overlapped, 0);
 }
 
 void Client::RecvPacket()
 {
+	DWORD recv_flag = 0;
+	memset(&recv.overlapped, 0, sizeof(recv.overlapped));
+	recv.wsaBuf.len = BUFFERSIZE - prevRemainData;
+	recv.wsaBuf.buf = recv.sendBuf + prevRemainData;
+	WSARecv(socket, &recv.wsaBuf, 1, 0, &recv_flag,	&recv.overlapped, 0);
+}
+
+void Client::SendServerLoginPacket(const int id)
+{
+	ServerLoginPacket packet;
+	packet.size = sizeof(ServerLoginPacket);
+	packet.type = ClientLogin;
+	packet.id = id;
+
+	SendPacket(&packet);
 }
--- PotaTongServer/PotaTongServer/ServerBase.h
+++ PotaTongServer/PotaTongServer/ServerBase.h
@@ -13,20 +13,26 @@ public:
 	void InitHandler();
 
 	void Accept();
+	void Reve(const int id, DWORD recvByte, OverlappedEx* overlappedEx);
 	void Disconnect(int ID);
+
+	void ProcessPacket(const int id, char* packet);
+	void ProcessInput(const int id, int key);
 
 private:
 	SOCKET ServerSocket, ClientSocket;
 	HANDLE IOCPHandle;
 	OverlappedEx GlobalOverlapped;
 
-	concurrency::concurrent_unordered_map<int, Client> clinets;
-	MatchingManager matchingManager;
+	concurrency::concurrent_unordered_map<int, Client> clients;
+	MatchingManager* matchingManager;
 
 	concurrency::concurrent_unordered_map<unsigned short,
 		std::function<void(unsigned char*)>> handlerMap;
 	concurrency::concurrent_unordered_map<unsigned short, GameMode*> gameRoomMap;
 
 	std::vector<thread> workerThreads;
+
+	atomic<int> clientID = 0;	
 };
 
