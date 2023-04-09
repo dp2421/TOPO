e8462f9b84545261a9250105d03e098b95753c92
민기 손 <meenki98@gmail.com>
2023-04-10 05:45:57 +0900

    Protocol 자료형, OPCODE 수정

--- Protocol.h
+++ Protocol.h
@@ -1,59 +1,59 @@
 #pragma once
 
-constexpr float DT = 1 / 60;
+constexpr float DT = 1.f / 60.f;
 constexpr float SPEED = 200.f;
 
-constexpr int PORTNUM = 4000;
+constexpr int PORTNUM = 20150;
 constexpr int BUFFERSIZE = 256;
 constexpr int NAMESIZE = 20;
 constexpr char SERVERIP[] = "127.0.0.1";
 
-constexpr char ClientLogin = 100;
-constexpr char ClientMatching = 101;
+constexpr unsigned char ClientLogin = 100;
+constexpr unsigned char ClientMatching = 101;
 // 캐릭바꾸기? 색바꾸기 고려
-constexpr char ClientReady = 102;
-constexpr char ClientKeyInput = 103;
+constexpr unsigned char ClientReady = 102;
+constexpr unsigned char ClientKeyInput = 103;
 
 //-------------------------------------------------------------------------------------
 
-constexpr char ServerLogin = 201;
+constexpr unsigned char ServerLogin = 201;
 constexpr char ServerMatchingOK = 202;
 constexpr char ServerGameStart = 203;
 
-constexpr char ServerAddPlayerInfo = 204;
-constexpr char ServerPlayerInfo = 205;
-constexpr char ServerObstacleInfo = 206;
-constexpr char ServerGameTimer = 207;
-constexpr char ServerGameEnd = 208;
+constexpr unsigned char ServerAddPlayer = 204;
+constexpr unsigned char ServerPlayerInfo = 205;
+constexpr unsigned char ServerObstacleInfo = 206;
+constexpr unsigned char ServerGameTimer = 207;
+constexpr unsigned char ServerGameEnd = 208;
 
-constexpr char ServerGameResult = 208;
+constexpr unsigned char ServerGameResult = 208;
 
 #pragma pack (push, 1)
 
 struct ClientLoginPacket
 {
 	unsigned char size;
-	char	type;
+	unsigned char	type;
 	char	name[NAMESIZE];
 };
 
 struct ClientMatchingPacket
 {
 	unsigned char size;
-	char	type;
+	unsigned char	type;
 	char	gameMode;
 };
 
 struct ClientReadyPacket
 {
 	unsigned char size;
-	char	type;
+	unsigned char	type;
 };
 
 struct ClientKeyInputPacket
 {
 	unsigned char size;
-	char	type;
+	unsigned char	type;
 	int		key;
 	// 플레이어가 바라보고 있는 방향 받아옴
 	float	x, y, z;
@@ -64,7 +64,7 @@ struct ClientKeyInputPacket
 struct ServerLoginPacket
 {
 	unsigned char size;
-	char	type;
+	unsigned char	type;
 	int		id;
 	float	x, y, z;
 };
@@ -72,19 +72,19 @@ struct ServerLoginPacket
 struct ServerMatchingOKPacket
 {
 	unsigned char size;
-	char	type;
+	unsigned char	type;
 };
 
 struct ServerGameStartPacket
 {
 	unsigned char size;
-	char	type;
+	unsigned char	type;
 };
 
 struct ServerAddPlayerPacket
 {
 	unsigned char size;
-	char	type;
+	unsigned char	type;
 	int		id;
 	float	x, y, z;
 };
@@ -92,7 +92,7 @@ struct ServerAddPlayerPacket
 struct ServerPlayerInfoPacket
 {
 	unsigned char size;
-	char	type;
+	unsigned char	type;
 	int		id;
 	float	xPos, yPos, zPos;
 	float	xDir, yDir, zDir;
@@ -102,7 +102,7 @@ struct ServerPlayerInfoPacket
 struct ServerObstacleInfoPacket
 {
 	unsigned char size;
-	char	type;
+	unsigned char	type;
 	//타입에 따라서 내용이 달라질 것 같음
 	//장애물에 번호 부여?
 	//NPC같은 느낌으로 따로 클래스?
@@ -111,21 +111,21 @@ struct ServerObstacleInfoPacket
 struct ServerGameTimerPacket
 {
 	unsigned char size;
-	char	type;
+	unsigned char	type;
 	int		time;
 };
 
 struct ServerGameEndPacket
 {
 	unsigned char size;
-	char	type;
+	unsigned char	type;
 	char	gameMode;
 };
 
 struct ServerGameResultPacket
 {
 	unsigned char size;
-	char	type;
+	unsigned char	type;
 	int		id;
 	// 랭킹만? 스코어만? 둘다?
 };
