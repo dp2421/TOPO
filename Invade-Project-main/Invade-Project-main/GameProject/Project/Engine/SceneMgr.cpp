bb51ed690e3b30510d3239501c247b8555163b6e
eAg302 <65862680+eAg302@users.noreply.github.com>
2023-07-31 06:59:02 +0900

    UI 위치수정..

--- Invade-Project-main/Invade-Project-main/GameProject/Project/Engine/SceneMgr.cpp
+++ Invade-Project-main/Invade-Project-main/GameProject/Project/Engine/SceneMgr.cpp
@@ -1706,7 +1706,7 @@ void CSceneMgr::InitUI()
 	pObject->AddComponent(new CUIScript);
 	pObject->GetScript<CUIScript>()->SetType(UI_TYPE::SELECT_BUTTON);
 	// Transform ����
-	pObject->Transform()->SetLocalPos(Vec3(-winsize.x/2, winsize.y/16, 0.f));
+	pObject->Transform()->SetLocalPos(Vec3(-winsize.x/2, winsize.y/7, 0.f));
 	pObject->Transform()->SetLocalRot(Vec3(XM_PI, 0.f, XM_PI));
 	pObject->Transform()->SetLocalScale(Vec3(winsize.x/4, winsize.y/12, 1.f));
 	// MeshRender ����
@@ -1715,7 +1715,7 @@ void CSceneMgr::InitUI()
 	pObject->MeshRender()->GetCloneMaterial()->SetData(SHADER_PARAM::TEX_0, pStartButton.GetPointer());
 	// Collider2D
 	pObject->Collider2D()->SetCollider2DType(COLLIDER2D_TYPE::RECT);
-	pObject->Collider2D()->SetOffsetPos(Vec3(-winsize.x / 2, winsize.y / 8, 0.f));
+	pObject->Collider2D()->SetOffsetPos(Vec3(-winsize.x / 3, winsize.y / 7, 0.f));
 	m_pStartScene->FindLayer(L"UI")->AddGameObject(pObject, m_pStartScene);
 
 
@@ -1729,7 +1729,7 @@ void CSceneMgr::InitUI()
 	pObject->GetScript<CUIScript>()->SetType(UI_TYPE::TITLE);
 
 	// Transform ����
-	pObject->Transform()->SetLocalPos(Vec3(0, winsize.y / 4, 0.f));
+	pObject->Transform()->SetLocalPos(Vec3(0, winsize.y / 3, 0.f));
 	pObject->Transform()->SetLocalRot(Vec3(XM_PI, 0.f, XM_PI));
 	pObject->Transform()->SetLocalScale(Vec3(winsize.x*1.2 , winsize.x*1.2, 1.f));
 	// MeshRender ����
@@ -1753,7 +1753,7 @@ void CSceneMgr::InitUI()
 	pObject->GetScript<CUIScript>()->SetType(UI_TYPE::WINDOW);
 
 	// Transform ����
-	pObject->Transform()->SetLocalPos(Vec3(0, winsize.y/4, 0.f));
+	pObject->Transform()->SetLocalPos(Vec3(0, winsize.y/3, 0.f));
 	pObject->Transform()->SetLocalRot(Vec3(XM_PI, 0.f, XM_PI));
 	pObject->Transform()->SetLocalScale(Vec3(winsize.x*1.5, winsize.y/2.f, 1.f));
 	// MeshRender ����
